2373da082eaf65c57ca3850d8220cb5bedddcfcd
Jacartas <goncalomatospereira@gmail.com>
2021-12-08 18:59:24 +0000

    das

--- src/StorageNode.java
+++ src/StorageNode.java
@@ -1,9 +1,13 @@
 import java.io.IOException;
-		import java.io.InputStreamReader;
-		import java.io.OutputStreamWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
 		import java.io.PrintWriter;
 		import java.net.InetAddress;
-		import java.net.Socket;
+import java.net.ServerSocket;
+import java.net.Socket;
 		import java.net.UnknownHostException;
 		import java.nio.file.Files;
 		import java.util.ArrayList;
@@ -25,10 +29,9 @@ public class StorageNode {
 	private String portoDiretorio;
 	private String ficheiro;
 	private InetAddress ip;
+	private ServerSocket serverSocket;
 
-
-
-	public StorageNode(String ip,String portoDiretorio,String porto,String ficheiro) throws UnknownHostException {
+	public StorageNode(String ip,String portoDiretorio,String porto,String ficheiro) throws UnknownHostException,IOException {
 		this.ip=InetAddress.getByName(ip);
 		this.porto = porto;
 		this.portoDiretorio = "8080";
@@ -41,10 +44,11 @@ public class StorageNode {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		this.serverSocket = new ServerSocket(Integer.parseInt(porto));
 		new DataInjectionErrorThread().start();
+		serve();
 	}
-	public StorageNode(String ip, String portoDiretorio, String porto) throws UnknownHostException {
+	public StorageNode(String ip, String portoDiretorio, String porto) throws UnknownHostException,IOException {
 		this.porto = porto;
 		this.portoDiretorio = portoDiretorio;
 		this.ip = InetAddress.getByName(ip);
@@ -66,8 +70,9 @@ public class StorageNode {
 			for(int i=0;i<fileContents.length;i++)  storedData[i]= new CloudByte(fileContents[i]);
 			System.out.println("Loaded data from file:1000000");
 		} catch (IOException e) {e.printStackTrace();}
-
+		this.serverSocket = new ServerSocket(Integer.parseInt(porto));
 		new DataInjectionErrorThread().start();
+		serve();
 	}
 
 	public class Node{
@@ -82,8 +87,7 @@ public class StorageNode {
 	}
 
 	public List<Node> getNodes() throws IOException {
-		ArrayList<Node> nodesList = null;
-		
+		ArrayList<Node> nodesList=new ArrayList<>();		
 		System.out.println("getnodes");
 		out.println("nodes");
 		while(true) {
@@ -98,7 +102,6 @@ public class StorageNode {
 			portoNode= componentes[2];
 			System.out.println("Ip: "+ipNode+"  Porto: "+portoNode);
 			if (!this.porto.contentEquals(portoNode)) {
-				System.out.println("corri");
 				nodesList.add(new Node(ipNode, portoNode));
 			}			
 		}
@@ -119,12 +122,26 @@ public class StorageNode {
 		//System.out.println(in.read());
 	}
 
+	public void serve() {
+		System.err.println("Servia iniciar...");
+		while (true) {
+		  try {
+			while (true) {
+			  Socket s = this.serverSocket.accept();
+			  (new ResponderNodes(s)).start();
+			} 
+		  } catch (IOException e) {
+			System.err.println("Erro ao aceitar ligade cliente no diret");
+		  } 
+		} 
+	  }
+
 	public void runClient() {
 		try {
 			connectToServer();
 			registerInServer();
 			getNodes();
-			System.out.println("acabei getnodes");
+			new DownloadThread(InetAddress.getByName("127.0.0.1"), "8081").start();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -136,7 +153,7 @@ public class StorageNode {
 		}
 	}
 
-	public static void main(String[] args) throws UnknownHostException {
+	public static void main(String[] args) throws UnknownHostException,IOException {
 		if(args.length==4) new StorageNode(args[0],args[1],args[2],args[3]).runClient();
 		else if(args.length==3) new StorageNode(args[0],args[1],args[2]).runClient();
 		else System.err.println("Invalid arguments");
@@ -158,18 +175,53 @@ public class StorageNode {
 	}
 
 	public class DownloadThread extends Thread{
+		private ObjectInputStream in;
+		private ObjectOutputStream out;
+		private InetAddress ip;
+		private String porto;
+
 		public void run() {
-			Scanner s= new Scanner(System.in);
-			while(true) {
-				String error = s.nextLine();
-				String[] str=error.split(" ");
-				if(str[0].equals("ERROR") && str.length==2) {
-					int index= Integer.parseInt(str[1]);
-					storedData[index].makeByteCorrupt();
-					System.out.println("Error injected: "+storedData[index]+" Parity NOK");
-				}
+			try {
+				System.out.println("comecei contacto");
+				connectToNode();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+
 		}
+		
+		public DownloadThread(InetAddress ip, String porto) {
+			this.ip = ip;
+			this.porto = porto;
+		}
+
+		void connectToNode() throws IOException {
+			socket = new Socket(ip, Integer.parseInt(porto));
+			in = new ObjectInputStream(socket.getInputStream());
+			out = new ObjectOutputStream(socket.getOutputStream());
+		}		
+	}
+
+	public class ResponderNodes extends Thread {
+		private Socket clientSocket;
+        private ObjectInputStream in;
+        private ObjectOutputStream out;
+
+        public ResponderNodes(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        public void run() {
+            try {
+				out = new ObjectOutputStream(clientSocket.getOutputStream());
+				in = new ObjectInputStream(socket.getInputStream());
+			  	System.out.println("conectado");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}           
+    	}
 	}
 }
 
