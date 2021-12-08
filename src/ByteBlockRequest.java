ddcd5e5ab77f0622a67540531b75ad9c895464fa
ItzHardcore <bruno.vp.1999@gmail.com>
2021-12-08 16:32:00 +0000

    inicio da fase 4

---
+++ src/ByteBlockRequest.java
@@ -0,0 +1,25 @@
+public class ByteBlockRequest {
+    private int startIndex;
+    private int length;
+
+    public ByteBlockRequest(int startIndex, int length){
+        this.length = length;
+        this.startIndex = startIndex;
+    }
+
+    public int getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(int startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
--- src/StorageNode.java
+++ src/StorageNode.java
@@ -1,22 +1,23 @@
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.nio.file.Files;
+		import java.io.InputStreamReader;
+		import java.io.OutputStreamWriter;
+		import java.io.PrintWriter;
+		import java.net.InetAddress;
+		import java.net.Socket;
+		import java.net.UnknownHostException;
+		import java.nio.file.Files;
+		import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
+		import java.io.BufferedReader;
+		import java.io.BufferedWriter;
+		import java.io.File;
 
 public class StorageNode {
-	
-	public static final int MIN_NUMBER_ANSWER_TO_CORRECT = 2;
-	public static final int STORED_DATA_LENGTH = 1000000;
-	public static final int INITIAL_DOWNLOAD_BLOCK_LENGTH = 10000;
-	private CloudByte[] storedData = new CloudByte[STORED_DATA_LENGTH];
+
+	public static final int COMPRIMENTO_DADOS = 1000000;
+	private CloudByte[] storedData = new CloudByte[COMPRIMENTO_DADOS];
 	private BufferedReader in;
 	private PrintWriter out;
 	private Socket socket;
@@ -24,24 +25,107 @@ public class StorageNode {
 	private String portoDiretorio;
 	private String ficheiro;
 	private InetAddress ip;
+	private ArrayList<Node> nodesList;
 
-	
+
 	public StorageNode(String ip,String portoDiretorio,String porto,String ficheiro) throws UnknownHostException {
 		this.ip=InetAddress.getByName(ip);
 		this.porto = porto;
 		this.portoDiretorio = "8080";
 		this.ficheiro=ficheiro;
-		
 		System.out.println("Sending to diretory:"+" INSC "+ip+" "+porto);
 		try {
-			byte[] fileContents = Files.readAllBytes(new File(ficheiro).toPath());
-			for(int i=0;i<fileContents.length;i++)  storedData[i]= new CloudByte(fileContents[i]); 
+			byte[] conteudoFicheiro = Files.readAllBytes(new File(ficheiro).toPath());
+			for(int i=0;i<conteudoFicheiro.length;i++)  storedData[i]= new CloudByte(conteudoFicheiro[i]);
 			System.out.println("Loaded data from file:1000000");
-		} catch (IOException e) {e.printStackTrace();}
-		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		new DataInjectionErrorThread().start();
 	}
-	
+	public StorageNode(String porto, String portoDiretorio, InetAddress ip) {
+		this.porto = porto;
+		this.portoDiretorio = portoDiretorio;
+		this.ip = ip;
+
+		try {
+			//getnodes
+			//cria o array de pedidos
+
+			// creating object of List<String>
+			List<String> list = new ArrayList<String>();
+
+			// populate the list
+			list.add("A");
+			list.add("B");
+			list.add("C");
+			list.add("D");
+			list.add("E");
+
+			// printing the Collection
+			System.out.println("List : " + list);
+
+			// create a synchronized list
+			List<String> synlist = Collections
+					.synchronizedList(list);
+
+			// printing the Collection
+			System.out.println("Synchronized list is : " + synlist);
+
+
+			byte[] fileContents = Files.readAllBytes(new File(ficheiro).toPath());
+			for(int i=0;i<fileContents.length;i++)  storedData[i]= new CloudByte(fileContents[i]);
+			System.out.println("Loaded data from file:1000000");
+		} catch (IOException e) {e.printStackTrace();}
+
+		new DataInjectionErrorThread().start();
+	}
+
+	public class Node{
+		InetAddress ip;
+		String porto;
+
+		public Node(InetAddress ip, String porto) {
+			super();
+			this.ip = ip;
+			this.porto = porto;
+		}
+	}
+
+	public StorageNode(String ip,String portoDiretorio,String porto) throws UnknownHostException {
+		this.ip=InetAddress.getByName(ip);
+		this.porto = porto;
+		this.portoDiretorio = "8080";
+		//apanhar byte
+		System.out.println("Sending to diretory:"+" INSC "+ip+" "+porto);
+		try {
+			byte[] conteudoFicheiro = Files.readAllBytes(new File(ficheiro).toPath());
+			for(int i=0;i<conteudoFicheiro.length;i++)  storedData[i]= new CloudByte(conteudoFicheiro[i]);
+			System.out.println("Loaded data from file:1000000");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		new DataInjectionErrorThread().start();
+	}
+
+	public void getNodes() throws IOException {
+		System.out.println("getnodes");
+		out.println("nodes");
+		while(true) {
+			String msg =in.readLine();
+			if(msg.equals("END"))
+				break;
+			InetAddress ip;
+			String porto;
+			String[] componentes = msg.split(" ");
+			ip= InetAddress.getByName(componentes[1].substring(1));
+			porto= componentes[2];
+			nodesList.add(new Node(ip, porto));
+		}
+	}
+
 	void connectToServer() throws IOException {
 		socket = new Socket(ip, Integer.parseInt(portoDiretorio));
 		in = new BufferedReader(new InputStreamReader(
@@ -50,27 +134,32 @@ public class StorageNode {
 				new OutputStreamWriter(socket.getOutputStream())),
 				true);
 	}
-	
-	void sendMessages() throws IOException {
+
+	void registerInServer() throws IOException {
 		out.println("INSC "+ip+" "+porto);
-		System.out.println(in.read());
+		//System.out.println(in.read());
 	}
-	
+
 	public void runClient() {
 		try {
 			connectToServer();
-			sendMessages();
+			registerInServer();
+			getNodes();
+			System.out.println("acabei getnodes");
 		} catch (IOException e) {
+			e.printStackTrace();
 		} finally {
 			try {
 				socket.close();
 			} catch (IOException e) {
+
 			}
 		}
 	}
 
 	public static void main(String[] args) throws UnknownHostException {
 		if(args.length==4) new StorageNode(args[0],args[1],args[2],args[3]).runClient();
+		else if(args.length==3) new StorageNode(args[0],args[1],args[2]).runClient();
 		else System.err.println("Invalid arguments");
 	}
 
@@ -80,12 +169,13 @@ public class StorageNode {
 			while(true) {
 				String error = s.nextLine();
 				String[] str=error.split(" ");
-				if(str[0].equals("ERROR") && str.length==2) { 
+				if(str[0].equals("ERROR") && str.length==2) {
 					int index= Integer.parseInt(str[1]);
-					System.out.println("Error injected:"+storedData[index]+" Parity NOK");
 					storedData[index].makeByteCorrupt();
+					System.out.println("Error injected: "+storedData[index]+" Parity NOK");
 				}
 			}
 		}
 	}
 }
+
